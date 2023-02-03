31ef2e496477eebe71914bcbaf7689d7e874e4e8
sohith0912 <123440640+sohith0912@users.noreply.github.com>
2023-02-03 16:47:29 -0600

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,5 @@
+#QUESTON2
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# stat from index 1 with step 2( means 1, 3, 5, an so on)
+for i in my_list[1::2]:
+    print(i, end=" ")
---
+++ q3.py
@@ -0,0 +1,7 @@
+#QUESTION3
+x = [23, 'python', 23.98]
+n = []
+for i in (x):
+    n.append(type(i))
+print(x)
+print(n)
